e3f2fb82f4641d43e4da6d5fc10158609d812f15
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-04-07 19:06:42 +0700

    input data menggunakan getline

--- structgetline.cpp
+++ structgetline.cpp
@@ -14,3 +14,19 @@ struct Orang
     detaiAlamat alamat;
     int umur;
 };
+
+int main ()
+{
+    // Deklarasi variabel struct
+    Orang mhs;
+    // Mengisi data
+    cout << "Isikan data nama : ";
+    getline(cin, mhs.nama);
+    cout << "Isikan data desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Isikan data kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+}
